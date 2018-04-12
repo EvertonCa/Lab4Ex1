c41f59efac8fdd804cd914b91d5c5e8b9be079ac
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-04-12 16:18:58 -0300

    Comentários adicionados

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+Aluno: Everton Cardoso Acchetta
+RA: 22.117.061-6
+Disciplina: CC3642 - Orientação a Objetos
+Programa criado para receber dois conjuntos de numeros inteiros entre 0 e 100 e manipula-los
+ */
+
 #include <iostream>
 #include "IntegerSet.h"
 
@@ -7,7 +14,8 @@ int main()
 {
     vector<int> vetorTemp;
     int temp;
-
+        
+    //mensagem de boas vindas e preenchimento do primeiro conjunto
     cout << "|--------------------------------------------------------------------------|" << endl;
     cout << "|                     BEM VINDO AO VECTOR ANALIST 3000                     |" << endl;
     cout << "|--------------------------------------------------------------------------|" << endl << endl;
@@ -19,6 +27,7 @@ int main()
 
     cin >> temp;
 
+    //preenchimento usando o construtor que passa um vetor de inteiros para a criação do conjunto
     while(temp >= 0 && temp <= 100)
     {
         vetorTemp.push_back(temp);
@@ -29,6 +38,7 @@ int main()
 
     vetorTemp.clear();
 
+    //mensagem de preenchimento do segundo conjunto
     cout << "|--------------------------------------------------------------------------|" << endl;
     cout << "|                             SEGUNDO CONJUNTO                             |" << endl;
     cout << "| Entre com os inteiros que deseja armazenar (entre 0 e 100) sem repeticao:|" << endl;
@@ -37,8 +47,9 @@ int main()
 
     cin >> temp;
 
+    //preenchimento usando o construtor padrão e adicionando cada numero no conjunto
     IntegerSet segundoVetor;
-
+    
     while(temp >= 0 && temp <= 100)
     {
         segundoVetor.insertElement(temp);
@@ -47,9 +58,10 @@ int main()
 
     bool teste = true;
 
-    while (teste)
+    while (teste) //loop de execução infinita até que seja solicitado fim do programa
     {
         int opcao;
+        //menu de opções para o usuário
         cout << "|--------------------------------------------------------------------------|" << endl;
         cout << "| Entre com a opcao desejada:                                              |" << endl;
         cout << "| 1 - Uniao dos conjuntos:                                                 |" << endl;
@@ -65,7 +77,7 @@ int main()
 
         switch (opcao)
         {
-            case 0:
+            case 0: //FIM DO PROGRAMA
             {
                 cout << "|--------------------------------------------------------------------------|" << endl;
                 cout << "|                 Obrigado por usar o VECTOR ANALIST 3000!                 |" << endl;
@@ -74,21 +86,21 @@ int main()
                 break;
             }
 
-            case 1:
+            case 1: //monstra a união entre os dois conjuntos
             {
                 IntegerSet terceiroVetor = primeiroVetor.unionOfSets(segundoVetor);
                 terceiroVetor.printSet();
                 break;
             }
 
-            case 2:
+            case 2: //mostra a intersecção entre os dois conjuntos
             {
                 IntegerSet terceiroVetor = primeiroVetor.intersectionOfSets(segundoVetor);
                 terceiroVetor.printSet();
                 break;
             }
 
-            case 3:
+            case 3: // verifica se os dois conjuntos são iguais (contém os mesmos numeros)
             {
                 bool igual = primeiroVetor.isEqualTo(segundoVetor);
                 if(igual)
@@ -99,19 +111,19 @@ int main()
                 break;
             }
 
-            case 4:
+            case 4: //imprime o primeiro conjunto
             {
                 primeiroVetor.printSet();
                 break;
             }
 
-            case 5:
+            case 5: //inmprime o segundo conjunto
             {
                 segundoVetor.printSet();
                 break;
             }
                 
-            case 6:
+            case 6: //imprime o primeiro conjunto após perguntar qual numero do mesmo deve ser apagado e em seguida, o apaga.
             {
                 cout << "|--------------------------------------------------------------------------|" << endl;
                 cout << "|                  Qual dos numeros abaixo deseja apagar?                  |" << endl;
@@ -130,6 +142,7 @@ int main()
                 break;
             }
                 
+            //imprime o segundo conjunto após perguntar qual numero do mesmo deve ser apagado e em seguida, o apaga.
             case 7:
             {
                 cout << "|--------------------------------------------------------------------------|" << endl;
@@ -149,6 +162,7 @@ int main()
                 break;
             }
 
+            // mensagem de opção inválida
             default:
             {
                 cout << "|--------------------------------------------------------------------------|" << endl;
