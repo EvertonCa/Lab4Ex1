ca04ed740398813a214ffcfd4d54f5376bff994d
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-04-12 16:23:17 -0300

    Comentários adicionados

--- Lib/IntegerSet.cpp
+++ Lib/IntegerSet.cpp
@@ -1,15 +1,18 @@
-//
-// Created by Everton Cardoso on 28/03/2018.
-//
+/*
+Aluno: Everton Cardoso Acchetta
+RA: 22.117.061-6
+Disciplina: CC3642 - Orientação a Objetos
+Programa criado para receber dois conjuntos de numeros inteiros entre 0 e 100 e manipula-los
+ */
 
 #include "IntegerSet.h"
 
-IntegerSet::IntegerSet()
+IntegerSet::IntegerSet() //função que retorna a união dos dois conjuntos
 {
     vetor.resize(101);
 };
 
-IntegerSet::IntegerSet(vector<int> vetorDeInteiros, unsigned long tamanho)
+IntegerSet::IntegerSet(vector<int> vetorDeInteiros, unsigned long tamanho) //construtor que recebe um vetor de inteiros e os usa para o objeto criado
 {
     for (int i = 0; i <= 100; ++i)
     {
@@ -22,6 +25,7 @@ IntegerSet::IntegerSet(vector<int> vetorDeInteiros, unsigned long tamanho)
     }
 };
 
+//função que retorna a união dos dois conjuntos
 IntegerSet IntegerSet::unionOfSets(IntegerSet segundoVetor)
 {
     IntegerSet terceiroVetor;
@@ -35,6 +39,7 @@ IntegerSet IntegerSet::unionOfSets(IntegerSet segundoVetor)
     return terceiroVetor;
 }
 
+//função que retorna a intersecção dos dois conjuntos
 IntegerSet IntegerSet::intersectionOfSets(IntegerSet segundoVetor)
 {
     IntegerSet terceiroVetor;
@@ -48,16 +53,19 @@ IntegerSet IntegerSet::intersectionOfSets(IntegerSet segundoVetor)
     return terceiroVetor;
 }
 
+//insere um elemento no conjunto
 void IntegerSet::insertElement(int k)
 {
     vetor[k] = 1;
 }
 
+//deleta um elemento do conjunto
 void IntegerSet::deleteElement(int k)
 {
     vetor[k] = 0;
 }
 
+//imprime o conjunto
 void IntegerSet::printSet()
 {
     int contador = 0;
@@ -76,6 +84,7 @@ void IntegerSet::printSet()
     cout<<endl;
 }
 
+//retorna verdadeiro caso os dois conjuntos sejam iguais
 bool IntegerSet::isEqualTo(IntegerSet segundoVetor)
 {
     bool igual = true;
